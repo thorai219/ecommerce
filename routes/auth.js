53d41599fc702c81b76ebde1d3cca28ce199ec12
Terry Lee <slsk0219@gmail.com>
2020-11-27 08:33:01 -0800

    need more routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,31 +1,34 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-const axios = require('axios');
+const axios = require("axios");
 
-router.post('/login', (req, res, next) => {
-  try{
-    const data = JSON.stringify({"password": `${req.body.password}`,"username": `${req.body.username}`});
+router.post("/login", (req, res, next) => {
+  try {
+    const data = JSON.stringify({
+      password: `${req.body.password}`,
+      username: `${req.body.username}`,
+    });
     const config = {
-      method: 'post',
-      url: 'https://sandbox-api.violet.io/v1/login',
-      headers: { 
-        'X-Violet-App-Secret': '21a81126a67a4bc5964a4db72933ce57', 
-        'X-Violet-App-Id': '10121', 
-        'Content-Type': 'application/json'
+      method: "post",
+      url: "https://sandbox-api.violet.io/v1/login",
+      headers: {
+        "X-Violet-App-Secret": "21a81126a67a4bc5964a4db72933ce57",
+        "X-Violet-App-Id": "10121",
+        "Content-Type": "application/json",
       },
-      data : data
+      data: data,
     };
     axios(config)
-    .then(function (response) {
-      return res.json(JSON.stringify(response.data));
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-  }catch(err) {
-    return next(err)
+      .then(function (response) {
+        return res.json(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  } catch (err) {
+    return next(err);
   }
-})
+});
 
-module.exports = router
+module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require("./app");
 
 app.listen(8080, function () {
-  console.log("Listening on 3000");
+  console.log("Listening on 8080");
 });
